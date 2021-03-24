60a2d8f7a9c93a203487e5d14aecb600a2d1d4c9
jorgeiaguerrevere <jorgeiaguerrevere@penguin.lxd>
2021-03-24 07:13:26 +0000

    fixed form validation

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 function compute() {
-    p = document.getElementById("principal").value;
-    r = document.getElementById("rate").value;
-    y = document.getElementById("years").value;
-    d = new Date();
-    return_year = parseInt(d.getFullYear())+parseInt(y);
+    p = document.getElementById("principal");
+    r = document.getElementById("rate");
+    y = document.getElementById("years");
 
-    i = p * r / 100 * y ;
+    // Check principal field has a value
+    if(p.value == ""){
+        alert("Please enter an Amount");
+        p.focus();
+        return false;
+    }
+    // check if principal field has a positive value
+    if(parseFloat(p.value) <= 0){
+        alert("Please enter a positive Amount");
+        p.focus();
+        return false;
+    }
+
+    d = new Date();
+    return_year = parseInt(d.getFullYear())+parseInt(y.value);
+
+    i = p.value * r.value / 100 * y.value ;
 
     output_element = document.getElementById("calculation_output");
-    output_element.innerHTML = "If you deposit "+this.p+",<br>";
-    output_element.innerHTML = output_element.innerHTML + "at an interest rate of "+this.r+"%.<br>";
+    output_element.innerHTML = "If you deposit "+this.p.value+",<br>";
+    output_element.innerHTML = output_element.innerHTML + "at an interest rate of "+this.r.value+"%.<br>";
     output_element.innerHTML = output_element.innerHTML + "You will receive an amount of "+this.i+",<br>";
     output_element.innerHTML = output_element.innerHTML + "in the year "+this.return_year;
     return false;
